082c99e181f1c5d0a1094b06dd6a36675a0842e1
Chung Heon Lee <55828069+chungchunglee@users.noreply.github.com>
2021-07-24 14:18:33 +0900

    LCH_커밋

---
+++ Assets/Script/SceneManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            if (Input.GetMouseButtonDown(0))    // 추후 메뉴 버튼을 누르면 전환
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene("InGame");
+            }
+        }
+        else if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            if (Input.GetMouseButtonDown(0))    // 추후 메뉴 버튼을 누르면 전환
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene("MainWindow");
+            }
+        }
+        else
+        {
+            
+        }
+    }
+}
