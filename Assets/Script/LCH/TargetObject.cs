1e0a05d6110e7bed95e3bdf1f959057b6952dc9b
ChungChungLee <fire6376@gmail.com>
2021-07-30 00:19:18 +0900

    데미지구현중

--- Assets/Script/LCH/TargetObject.cs
+++ Assets/Script/LCH/TargetObject.cs
@@ -1,9 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Security;
 using UnityEngine;
 
 public class TargetObject : MonoBehaviour
 {
+    [SerializeField] protected ObjectInfo objectInfo;//https://everyday-devup.tistory.com/53 참조 
+    [SerializeField] protected GameObject destroyEffect;
+
+    protected bool isEnemy;
+
+    protected float hp;
+
+    public bool isNextTarget;
+
+    public ObjectInfo Info
+    {
+        get
+        {
+            return objectInfo;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
