c4a370a4651bf08c1781daae7b75704d6783f1af
chungchunglee <fire6376@gmail.com>
2021-08-02 17:03:58 +0900

    간단한 공격 알고리즘 구상

--- Assets/Script/LCH/TargetObject.cs
+++ Assets/Script/LCH/TargetObject.cs
@@ -1,3 +1,63 @@
+// 인식거리 & 사정거리
+// 자동 이동(바라보는 방향 + 이동속도)
+// 사정거리 - 정지 + 공격 // OnTriggerEnter
+// 사정거리 벗어남 - 이동 재개 // OnTriggerExit
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Security;
+using UnityEngine;
+
+public class TargetObject : MonoBehaviour
+{
+
+    protected bool isEnemy;
+
+    protected float hp;
+    protected float attack;
+    public float attackSpeed;
+    public bool isAttacked;
+    private float localTime;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void onDamaged(float damage)
+    {
+        hp -= damage;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(isAttacked&&other.tag == "Player")
+        {
+            other.gameObject.GetComponent<TargetObject>.onDamaged(attack);
+            //에니메이션
+            ~isAttacked;
+        }
+        else
+        {
+            localTime+=Time.deltaTime;
+            if(localTime>=attackSpeed)
+            {
+                ~isAttacked;
+                localTime = 0;
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //보는 방향으로 이동
+    }
+}
+
+/*
 using System.Collections;
 using System.Collections.Generic;
 using System.Net.Security;
@@ -33,3 +93,5 @@ public class TargetObject : MonoBehaviour
         
     }
 }
+
+*/
